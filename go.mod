13203485bbc80dcb7c1f06eb5b839f3af47f2412
Anton Skvartsou <antonskwr@gmail.com>
2021-03-17 18:37:44 +0100

    [wip] minimal working client

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/antonskwr/nat-punch-through-client
 
 go 1.14
+
+require github.com/libp2p/go-reuseport v0.0.2
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"os"
 	"strings"
 
+	"github.com/antonskwr/nat-punch-through-client/host"
 	"github.com/antonskwr/nat-punch-through-client/util"
+
+	reuse "github.com/libp2p/go-reuseport"
 )
 
 func GetRemoteTCPAddress() *net.TCPAddr {
@@ -70,13 +73,11 @@ func DialHubTCP() {
 	}
 }
 
-func DialHubUDP() {
+func DialHubUDP(hostport string, localPort int, name string) {
 	lAddr := net.UDPAddr{}
-	lAddr.Port = 9000
+	lAddr.Port = localPort
 
-	hubAddr := GetRemoteUDPAddress()
-
-	conn, err := net.DialUDP("udp", &lAddr, hubAddr)
+	conn, err := reuse.Dial("udp", lAddr.String(), hostport)
 	if err != nil {
 		util.HandleErr(err)
 		return
@@ -84,10 +85,14 @@ func DialHubUDP() {
 
 	defer conn.Close()
 
-	fmt.Printf("UDP: Successfully connected to Hub at %s\n", conn.RemoteAddr().String())
+	fmt.Printf("UDP: Successfully connected to %s at %s\n", name, conn.RemoteAddr().String())
 	fmt.Println("Type <STOP> to close the connection")
 	fmt.Println("Type <LIST> to list available hosts")
 	fmt.Println("Type <ADD [id]> to register host with id")
+	fmt.Println("Type <CONN [id]> to connect to host with id")
+	fmt.Println("Type <PULL> to pull msgs from connection (debug only)")
+
+	var interruptResp Resp
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
@@ -105,16 +110,18 @@ func DialHubUDP() {
 			return
 		}
 
-		msgData := []byte(text) // NOTE(antonskwr): flush text down the connection
-		_, err = conn.Write(msgData) // TODO(antonskwr): handle number of bytes written
+		if strings.TrimSpace(text) != "PULL" {
+			msgData := []byte(text)      // NOTE(antonskwr): flush text down the connection
+			_, err = conn.Write(msgData) // TODO(antonskwr): handle number of bytes written
 
-		if err != nil {
-			util.HandleErr(err)
-			continue
+			if err != nil {
+				util.HandleErr(err)
+				continue
+			}
 		}
 
 		incomingBuffer := make([]byte, 1024)
-		n, _, err := conn.ReadFromUDP(incomingBuffer)
+		n, err := conn.Read(incomingBuffer)
 		if err != nil {
 			util.HandleErr(err)
 			continue
@@ -122,33 +129,156 @@ func DialHubUDP() {
 
 		trimmedBuffer := strings.TrimSpace(string(incomingBuffer[0:n]))
 		fmt.Printf("HUB reply: %s\n", trimmedBuffer)
+
+		resp := handleMsgFromPacket(trimmedBuffer)
+		if resp.rType != RespTypeContinue {
+			interruptResp = resp
+			break
+		}
 	}
+
+	switch interruptResp.rType {
+	case RespTypeOk:
+		for !PingUDP(interruptResp.payload, localPort) {
+		}
+		DialHubUDP(interruptResp.payload, localPort, "host")
+	case RespTypeReq:
+		for !PingUDP(interruptResp.payload, localPort) {
+		}
+		fmt.Println("Was able to connect to client")
+	}
+}
+
+func PingUDP(hostport string, localPort int) bool {
+	lAddr := net.UDPAddr{}
+	lAddr.Port = localPort
+
+	conn, err := reuse.Dial("udp", lAddr.String(), hostport)
+	if err != nil {
+		util.HandleErr(err)
+		return false
+	}
+
+	defer conn.Close()
+
+	fmt.Println("PingUDP connection successful")
+
+	return true
+}
+
+type RespType int
+
+const (
+	RespTypeOk RespType = iota
+	RespTypeReq
+	RespTypeContinue
+)
+
+type Resp struct {
+	rType   RespType
+	payload string
+}
+
+func handleMsgFromPacket(msg string) Resp {
+	splittedMsgs := strings.Split(msg, " ")
+
+	if len(splittedMsgs) == 2 {
+		if splittedMsgs[0] == "OK" {
+			hostport := splittedMsgs[1]
+			host, port, err := net.SplitHostPort(hostport)
+			if err != nil {
+				util.HandleErr(err, "failed to parse hostport %s", hostport)
+				return Resp{RespTypeContinue, ""}
+			}
+
+			if len(host) != 0 && len(port) != 0 {
+				return Resp{RespTypeOk, hostport}
+			}
+		}
+		if splittedMsgs[0] == "REQ" {
+			hostport := splittedMsgs[1]
+			host, port, err := net.SplitHostPort(hostport)
+			if err != nil {
+				util.HandleErr(err, "failed to parse hostport %s", hostport)
+				return Resp{RespTypeContinue, ""}
+			}
+
+			if len(host) != 0 && len(port) != 0 {
+				return Resp{RespTypeReq, hostport}
+			}
+		}
+	}
+
+	return Resp{RespTypeContinue, ""}
 }
 
 func HubInvalidOption() {
 	fmt.Println("Invalid option")
 }
 
+func promptAddr() (string, error) {
+	fmt.Printf("enter address: ")
+	var addr string
+	_, err := fmt.Scanf("%s\n", &addr)
+	if err != nil {
+		return "", err
+	}
+	return addr, nil
+}
+
+func promptPort() (int, error) {
+	fmt.Printf("enter port: ")
+	var port int
+	_, err := fmt.Scanf("%d\n", &port)
+	if err != nil {
+		return 0, err
+	}
+	return port, nil
+}
+
 func promptUser() {
 	var input string
 
-	fmt.Printf("What would you like to do? dial (t)cp, dial (u)dp: ")
+	fmt.Println("What would you like to do?")
+	fmt.Println("(s)tart server at [port]")
+	fmt.Println("(d)ial hub at [hostport] from [port]")
+	// fmt.Println("(di)al hub at [hostname] from [port]")
+	fmt.Printf("-> ")
 	fmt.Scanf("%s\n", &input)
 
 	switch input {
-	case "t":
-		DialHubTCP()
-	case "u":
-		DialHubUDP()
-	// NOTE(antonskwr): more parameters prompting
-	// case "c":
-	// 	fmt.Println("Connect selected, enter server id:")
-	// 	var id uint32
-	// 	// TODO(antonskwr): handle negative values and bigger than max uint32
-	// 	// in golang will be 0 if overflood
-	// 	fmt.Scanf("%d\n", &id)
-	// 	connectCtx := Context {id}
-	// 	return HubConnectToServer, connectCtx
+	case "s":
+		port, err := promptPort()
+		if err != nil {
+			util.HandleErr(err)
+			return
+		}
+		host.StartUDPServer(port)
+	case "d":
+		hostport, err := promptAddr()
+		if err != nil {
+			util.HandleErr(err)
+			return
+		}
+		port, err := promptPort()
+		if err != nil {
+			util.HandleErr(err)
+			return
+		}
+		DialHubUDP(hostport, port, "Hub")
+		// case "di":
+		// hostport, err := promptAddr()
+		// if err != nil {
+		// 	util.HandleErr(err)
+		// 	return
+		// }
+		// // TODO: lookup ip
+		// port, err := promptPort()
+		// if err != nil {
+		// 	util.HandleErr(err)
+		// 	return
+		// }
+		// DialHubUDP(hostport, port)
 	default:
 		fmt.Println("Invalid option")
 	}
